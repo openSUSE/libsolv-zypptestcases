5d18afd1fcb76f4720ae4cb6b6d16b7d35240c32
Michael Schroeder <mls@suse.de>
2015-02-18 13:58:51 +0100

    add test for "install name nonexistant"

--- basic-exercises/Bug299486-test.r
+++ basic-exercises/Bug299486-test.r
@@ -1,2 +1,4 @@
 problem c43b1300 info nothing provides requested asdfasdf
+problem c43b1300 info package qwertyqwerty does not exist
+problem c43b1300 solution 0805f7b4 deljob install name qwertyqwerty
 problem c43b1300 solution b1a2ef67 deljob install provides asdfasdf
--- basic-exercises/Bug299486-test.t
+++ basic-exercises/Bug299486-test.t
@@ -2,4 +2,5 @@ repo system 0 testtags exercise-1-system.repo.gz
 repo test 0 testtags require-packages.repo.gz
 system i686 rpm system
 job install provides asdfasdf
+job install name qwertyqwerty
 result transaction,problems Bug299486-test.r
