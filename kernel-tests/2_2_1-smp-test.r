417758e7e60ff76acd28401bfbfa10736e2e9472
Michael Schroeder <mls@suse.de>
2017-10-18 12:57:07 +0200

    Update testcase to match current solver behaviour

--- kernel-tests/2_2_1-smp-test.r
+++ kernel-tests/2_2_1-smp-test.r
@@ -1,4 +1,6 @@
-problem 39270850 info nothing provides kernel(smp:mm) = 58746e28e1dfb1e0 needed by nvidia-gfx-kmp-smp-1.0.8178_2.6.16_rc5_git14_3c-1.i586
-problem 39270850 solution 32cf05d6 allow nvidia-gfx-kmp-smp-1.0.8178_2.6.16_rc5_git14_2-1.i586@system
-problem abddf85b info nothing provides kernel(smp:block) = ce47a299102c0f64 needed by cloop-kmp-smp-2.01_2.6.16_rc5_git14_3c-15.i586
-problem abddf85b solution 0cb412a2 allow cloop-kmp-smp-2.01_2.6.16_rc5_git14_2-15.i586@system
+problem 51d9a957 info nothing provides kernel(smp:block) = ce47a299102c0f64 needed by cloop-kmp-smp-2.01_2.6.16_rc5_git14_3c-15.i586
+problem 51d9a957 solution 2bb0eea1 allow cloop-kmp-smp-2.01_2.6.16_rc5_git14_2-15.i586@system
+problem 51d9a957 solution 45fe9815 erase cloop-kmp-smp-2.01_2.6.16_rc5_git14_2-15.i586@system
+problem f4e2a9ca info nothing provides kernel(smp:mm) = 58746e28e1dfb1e0 needed by nvidia-gfx-kmp-smp-1.0.8178_2.6.16_rc5_git14_3c-1.i586
+problem f4e2a9ca solution 06e84a6c erase nvidia-gfx-kmp-smp-1.0.8178_2.6.16_rc5_git14_2-1.i586@system
+problem f4e2a9ca solution d035dc9e allow nvidia-gfx-kmp-smp-1.0.8178_2.6.16_rc5_git14_2-1.i586@system
